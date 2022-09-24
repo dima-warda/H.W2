0741545c1a94ec6458285bef823197ffae11876b
Dima <daana1395@gmail.com>
2022-09-24 15:50:38 +0300

    .

---
+++ venv/2q4.3.py
@@ -0,0 +1,15 @@
+# 3: Unpack the tuple into 4 variables
+
+tuple1 = (10, 20, 30, 40)
+print(tuple1)
+for items in range (4) :
+    print(tuple1[items])
+
+# a,b,c,d = tuple1
+# print (tuple1)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+
